f6bd4246e9ea585340584b47231a9b48d46103ae
Uthmankontao <kontaoousmane40@gmail.com>
2025-09-18 17:08:53 +0200

    fix self.k, et ajouter des commentaires

--- Master_2_RL/TP1/forTP1.py
+++ Master_2_RL/TP1/forTP1.py
@@ -13,15 +13,20 @@ import matplotlib.pyplot as plt
 class Bandit:
     
     def __init__(self):
-        self.K=10
-        np.random.seed(5)
-        self.variance = np.random.randint(-1,2,size=self.K)
-        self.biais = np.random.poisson(3,self.K)
-        while sum(self.biais == self.biais.max()) != 1 :
-            self.biais = np.random.poisson(3,K)
+        self.K=10 # on crée un bandit avec 10 bras --> les 10 leviers du bandit manchot
+        np.random.seed(5) # pour obtr tjr le meme rslt.
+        self.variance = np.random.randint(-1,2,size=self.K) # ensuite dans un tbl de taille = au nbre de bras on mets
+        # aleatoirement des valeurs entieres entre -1 et 1
+        self.biais = np.random.poisson(3,self.K) # tableau de taille 10 tiré d'une loi poisson  de taille 3. cela fixe un biais de recompense pour chaque bras
+        while sum(self.biais == self.biais.max()) != 1 : # force la condition qu’un seul bras a le biais maximum (pas plusieurs)
+            self.biais = np.random.poisson(3,self.K)
   
-    def get_arm(self,k):   
-        reward  = np.random.poisson(6+self.variance[k]) + self.biais[k]  
+    def get_arm(self,k):
+        """simule la recompense quand on tire le bras k.
+        La récompense est un nombre aléatoire tiré d’une
+        loi de Poisson de moyenne 6 + variance[k], puis on ajoute le biais du bras k.""" 
+
+        reward  = np.random.poisson(6+self.variance[k]) + self.biais[k]
         return reward
 
 
@@ -30,7 +35,7 @@ if __name__ == "__main__":
     T=500
     bandits = [[]]
     for k in range(K-1):
-     bandits.append([]) 
+        bandits.append([]) # bandits est donc une liste de 10 listes dont une par bras pour stocker les récompenses observées
 
     myBandit = Bandit()
 
